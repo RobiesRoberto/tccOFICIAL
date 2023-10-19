4a53c7d66cce10163c799fb6be5eb7d1192b1538
RobiesRoberto <143709591+RobiesRoberto@users.noreply.github.com>
2023-10-19 17:01:02 -0300

    Add files via upload

---
+++ tcc-19-10-2/conexao.php
@@ -0,0 +1,14 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "robies";
+
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname); 
+
+try{
+    $conexao = new PDO("mysql:servidor=$servidor;dbname=" . $dbname, $usuario, $senha);
+    //echo "Conexão com banco de dados realizado com sucesso!";
+}catch(PDOException $err){
+    echo "Erro: Conexão com banco de dados não realizado com sucesso. Erro gerado " . $err->getMessage();
+}
